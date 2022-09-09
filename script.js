34257831396fa7b1fcbbc2bbcd7787534c40971b
Kushagra1974 <kamle.kush2002@gmail.com>
2022-09-10 00:02:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function btn_click() {
+    window.open("https://www.linkedin.com/in/kushagra-kamle-50737821b/", "_blank");
+}
+function alo_click(params) {
+    window.open("https://elegant-roentgen-87eeb8.netlify.app/", "_blank")
+}
+function covid_tracker_click(params) {
+    window.open("https://vigorous-mirzakhani-fca761.netlify.app/", "_blank")
+}
+function git_finder_click(params) {
+    window.open("https://inquisitive-baklava-f764c3.netlify.app/", "_blank")
+}
